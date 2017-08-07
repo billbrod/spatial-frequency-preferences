530c88e9340b1e01d9c07608081f86703204dd28
William F. Broderick <billbrod@gmail.com>
2017-08-07 17:16:27 -0400

    Adds aliasing check
    this commit adds functionality to check and visualize aliasing in the
    gratings:
    
     - adds scale_factor arg to log_polar_grating, which allows you to make
       a larger grating but scale the distances as if they were on a smaller
       grid
    
     - adds hidden function _create_better_sampled_grating, which uses
       scale_factor to create a version of the specified grating at a higher
       resolution
    
     - adds aliasing_plot, which takes in two gratings, one a better-sampled
       version of the other, and plots the specified slices in order to
       visualize any aliasing
    
     - adds check_aliasing, which wraps the previous two functions in order
       to simply create a low- and high-resolution grating (and returns
       them)

--- sfp/stimuli.py
+++ sfp/stimuli.py
@@ -3,9 +3,10 @@
 """
 import pyPyrTools as ppt
 import numpy as np
+from matplotlib import pyplot as plt
 
 
-def log_polar_grating(size, alpha, w_r=0, w_a=0, phi=0, ampl=1, origin=None):
+def log_polar_grating(size, alpha, w_r=0, w_a=0, phi=0, ampl=1, origin=None, scale_factor=1):
     """Make a sinusoidal grating in logPolar space.
 
     this allows for the easy creation of stimuli whose spatial frequency decreases with
@@ -40,8 +41,13 @@ def log_polar_grating(size, alpha, w_r=0, w_a=0, phi=0, ampl=1, origin=None):
 
     origin: 2-tuple of floats, the origin of the image, from which all distances will be measured
     and angles will be relative to. By default, the center of the image
+
+    scale_factor: int or float. how to scale the distance from the origin before computing the
+    grating. this is most often done for checking aliasing; e.g., set size_2 = 100*size_1 and
+    scale_factor_2 = 100*scale_factor_1. then the two gratings will have the same pattern, just
+    sampled differently
     """
-    rad = ppt.mkR(size, origin=origin)
+    rad = ppt.mkR(size, origin=origin)/scale_factor
     # if the origin is set such that it lies directly on a pixel, then one of the pixels will have
     # distance 0 and, if alpha is also 0, that means we'll have a -inf out of np.log2 and thus a
     # nan from the cosine. this little hack avoids that issue.
@@ -51,3 +57,65 @@ def log_polar_grating(size, alpha, w_r=0, w_a=0, phi=0, ampl=1, origin=None):
     theta = ppt.mkAngle(size, origin=origin)
 
     return ampl * np.cos((w_r/np.pi) * lrad + w_a * theta + phi)
+
+
+def _create_better_sampled_grating(orig_size, alpha, w_r=0, w_a=0, phi=0, ampl=1, orig_origin=None,
+                                   orig_scale_factor=1, check_scale_factor=99):
+    if check_scale_factor % 2 == 0:
+        raise Exception("For this aliasing check to work, the check_scale_factor must be odd!")
+    if orig_origin is None:
+        origin = None
+    else:
+        # this preserves origin's shape, regardless of whether it's an iterable or a scalar
+        origin = np.array(orig_origin) * check_scale_factor - (check_scale_factor - 1)/2
+    return log_polar_grating(orig_size*check_scale_factor, alpha, w_r, w_a, phi, ampl, origin,
+                             orig_scale_factor*check_scale_factor)
+
+
+def aliasing_plot(better_sampled_stim, stim, slices_to_check=None, axes=None, **kwargs):
+    """Plot to to check aliasing.
+
+    This does not create the stimuli, only plots them (see `check_aliasing` or `check_aliasing_with
+    mask` for functions that create the stimuli and then call this to plot them)
+
+    to add to an existing figure, pass axes (else a new one will be created)
+    """
+    size = stim.shape[0]
+    check_scale_factor = better_sampled_stim.shape[0] / size
+    if slices_to_check is None:
+        slices_to_check = [(size+1)/2]
+    elif not hasattr(slices_to_check, '__iter__'):
+        slices_to_check = [slices_to_check]
+    if axes is None:
+        fig, axes = plt.subplots(ncols=len(slices_to_check), squeeze=False,
+                                 figsize=(5*len(slices_to_check), 5), **kwargs)
+        # with squeeze=False, this will always be a 2d array, but because we only set ncols, it
+        # will only have axes in one dimension
+        axes = axes[0]
+    x0 = np.array(range(size)) / float(size) + 1./(size*2)
+    x1 = np.array(range(better_sampled_stim.shape[0])) / float(better_sampled_stim.shape[0]) + 1./(better_sampled_stim.shape[0]*2)
+    for i, ax in enumerate(axes):
+        ax.plot(x1, better_sampled_stim[:, check_scale_factor*slices_to_check[i] + (check_scale_factor - 1)/2])
+        ax.plot(x0, stim[:, slices_to_check[i]], 'o:')
+
+
+def check_aliasing(size, alpha, w_r=0, w_a=0, phi=0, ampl=1, origin=None, scale_factor=1,
+                   slices_to_check=None, check_scale_factor=99):
+    """Create a simple plot to visualize aliasing
+
+    arguments are mostly the same as for log_polar_grating. this creates both the specified
+    stimulus, `orig_stim`, and a `better_sampled_stim`, which has `check_scale_factor` more points
+    in each direction. both gratings are returned and a quick plot is generated.
+
+    NOTE that because this requires creating a much larger gradient, it can take a while. Reduce
+    `check_scale_factor` to speed it up (at the risk of your "ground truth" becoming aliased)
+
+    slices_to_check: list, None, or int. slices of the stimulus to plot. if None, will plot
+    center
+    """
+    orig_stim = log_polar_grating(size, alpha, w_r, w_a, phi, ampl, origin, scale_factor)
+    better_sampled_stim = _create_better_sampled_grating(size, alpha, w_r, w_a, phi, ampl, origin,
+                                                         scale_factor, check_scale_factor)
+    aliasing_plot(better_sampled_stim, orig_stim, slices_to_check)
+    return orig_stim, better_sampled_stim
+
