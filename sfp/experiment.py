3faa4f332f6c2045649ad0c9080b051a15c29834
William F. Broderick <billbrod@gmail.com>
2017-08-21 13:56:53 -0400

    modularizes experiment, adds expt function
    this commit adds several changes:
    
     - code has been modularized somewhat, creating a _set_params function
       which handles much of the a priori calculations and returns a couple
       dictionaries of parameters
    
     - renames main to run and adds expt function. what was the main
       function now called run because it does a single run. the expt
       function runs multiple runs and saves the data
    
     - changes how fixation dot is handled. instead of a 20% chance of
       changing every frame refresh, we now change every 1 to 3 seconds (by
       default, pull from a uniform distribution there)

--- sfp/experiment.py
+++ sfp/experiment.py
@@ -9,9 +9,45 @@ also make sure that you've already set up the monitor you'll be using in PsychoP
 
 import numpy as np
 from psychopy import visual, core, event
+import h5py
+import datetime
+import glob
 
 
-def main(stim_filename, monitor_kwargs={}, session_length=30, refresh_rate=60):
+def _set_params(stim_filename, session_length=30, refresh_rate=60, on_msec_length=300,
+                off_msec_length=200, fix_length_range=(1, 3), final_blank_sec_length=8,
+                size=[1080, 1080], monitor='test', units='pix', fullscr=True, screen=1,
+                **monitor_kwargs):
+    """set the various experiment parameters
+    """
+    stimuli = np.load(stim_filename)
+    expt_params = {'session_length_frames': session_length * refresh_rate}
+    expt_params['on_frame_length'] = np.round(on_msec_length / (1000. / refresh_rate))
+    expt_params['off_frame_length'] = np.round(off_msec_length / (1000. / refresh_rate))
+    # we want to convert the range of time that the fixation dot will remain the same color from
+    # seconds to frames
+    expt_params['fix_length_range'] = np.array(fix_length_range) * float(refresh_rate)
+    # the first dimension of stimuli (retrieved by len) is how many stimuli we have. the next two
+    # are the size of the stimuli
+    expt_params['stim_size'] = stimuli.shape[1:]
+    repeat_num = (session_length - final_blank_sec_length) / (len(stimuli) * ((on_msec_length + off_msec_length) / 1000.))
+    # with these two, we guarantee that stimuli will either end at the same time or before
+    # session_length
+    if repeat_num > 1:
+        # this rounds down if repeat_num is not an integer (i.e., repeat(2.5) is the same as
+        # repeat(2)), which is what we want because we'd rather have a bit of dead time
+        stimuli = np.tile(stimuli, (int(repeat_num), 1, 1))
+    elif repeat_num < 1:
+        stimuli = stimuli[:int(len(stimuli) * repeat_num)]
+
+    monitor_kwargs.update({'size': size, 'monitor': monitor, 'units': units, 'fullscr': fullscr,
+                           'screen': screen})
+    return stimuli, expt_params, monitor_kwargs
+
+
+def run(stim_filename, session_length=30, refresh_rate=60, on_msec_length=300, off_msec_length=200,
+        fix_length_range=(1, 3), final_blank_sec_length=8, fix_size=10, fix_color_change_pctg=.2,
+        **monitor_kwargs):
     """run one run of the experiment
 
     Before running this, you need to make sure the stimuli specified are in the correct order. This
@@ -29,47 +65,42 @@ def main(stim_filename, monitor_kwargs={}, session_length=30, refresh_rate=60):
     Arguments
     ============
 
+    stim_filename: string, path to .npy file where stimuli are stored (as 3d array)
+
     session_length: int, length in seconds
 
     refresh_rate: int or float, the refresh rate of the monitor, in Hz. If you don't know this,
     psychopy.info.RunTimeInfo() will give you the windowRefreshTime, and 1000 over that number is
     your refresh_rate. most monitors have a 60 Hz refresh rate, but the projecter at NYU's primsa
     scanner goes up to 360 Hz depending on the resolution
+
+    on_msec_length: int, length of the ON blocks in milliseconds; that is, the length of time to
+    display each stimulus before moving on
+
+    off_msec_length: int, length of the OFF blocks in milliseconds; that is, the length of time to
+    between stimuli
+
+    fix_length_range: 2-tuple of ints or floats. the length of time the fixation dot will remain
+    the same color is picked from a uniform distribution with these endpoints
+
+    fix_size: int, the size of the fixation dot, in monitor units (probably pixels, unless you set
+    units in the monitor_kwargs)
+
+    fix_color_change_pctg: float, the chance, on each frame, that the fixation dot will change
+    color
     """
-    stimuli = np.load(stim_filename)
-    session_length_frames = session_length * refresh_rate
-    on_frame_length = np.round(300 / (1000. / refresh_rate))
-    off_frame_length = np.round(100 / (1000. / refresh_rate))
-    # this is just going to be the refresh_rate, but if we decide to change it from 1 sec, this
-    # would do it
-    fix_change_min_length = np.round(1000 / (1000. / refresh_rate))
-    # the first dimension of stimuli (retrieved by len) is how many stimuli we have. the next two
-    # are the size of the stimuli
-    stim_size = stimuli.shape[1:]
-    repeat_num = session_length / (len(stimuli) * .4)
-    # with these two, we guarantee that stimuli will either end at the same time or before
-    # session_length
-    if repeat_num > 1:
-        # this rounds down if repeat_num is not an integer (i.e., repeat(2.5) is the same as
-        # repeat(2)), which is what we want because we'd rather have a bit of dead time
-        stimuli = np.tile(stimuli, (int(repeat_num), 1, 1))
-    elif repeat_num < 1:
-        stimuli = stimuli[:int(len(stimuli) * repeat_num)]
-    print len(stimuli), session_length_frames, on_frame_length, off_frame_length
+    stimuli, expt_params, monitor_kwargs = _set_params(stim_filename, session_length, refresh_rate,
+                                                       on_msec_length, off_msec_length,
+                                                       fix_length_range, final_blank_sec_length,
+                                                       **monitor_kwargs)
 
-    window_res = monitor_kwargs.get('size', [1080, 1080])
-    monitor_name = monitor_kwargs.get('monitor', 'test')
-    units = monitor_kwargs.get('units', 'pix')
-    fullscreen = monitor_kwargs.get('fullscr', True)
-    screen_num = monitor_kwargs.get('screen', 1)
+    win = visual.Window(**monitor_kwargs)
 
-    win = visual.Window(window_res, monitor=monitor_name, units=units,
-                        fullscr=fullscreen, screen=screen_num, **monitor_kwargs)
+    fixation = visual.GratingStim(win, size=fix_size, pos=[0, 0], color=(1, 0, 0), sf=0, mask='circle')
+    gratings = [visual.ImageStim(win, image=stim, size=expt_params['stim_size']) for stim in stimuli]
 
-    fixation = visual.GratingStim(win, size=10, pos=[0, 0], color=(1, 0, 0), sf=0, mask='circle')
-    gratings = [visual.ImageStim(win, image=stim, size=stim_size) for stim in stimuli]
-
-    wait_text = visual.TextStim(win, "Press 5 to start")
+    wait_text = visual.TextStim(win, ("Press 5 to start\nq will quit this run\nescape will quit "
+                                      "this session"))
     wait_text.draw()
     win.flip()
 
@@ -78,22 +109,25 @@ def main(stim_filename, monitor_kwargs={}, session_length=30, refresh_rate=60):
     all_keys = event.waitKeys(keyList=['5', 'q', 'escape'], timeStamped=clock)
     if 'q' in [k[0] for k in all_keys] or 'escape' in [k[0] for k in all_keys]:
         win.close()
-        return all_keys
+        return None, all_keys
     win.recordFrameIntervals = True
 
     last_fix_change = 0
+    next_fix_change = np.floor(np.random.uniform(*expt_params['fix_length_range']))
     last_stim_change = 0
     stim_num = 0
-    keys_pressed = all_keys
-    for frame_num in range(session_length_frames):
+    keys_pressed = [(key[0], key[1], -1) for key in all_keys]
+    fixation_dot = [(0, clock.getTime(), fixation.color)]
+    for frame_num in xrange(expt_params['session_length_frames']):
         if stim_num < len(stimuli-1):
-            if frame_num < (last_stim_change + on_frame_length):
+            if frame_num < (last_stim_change + expt_params['on_frame_length']):
+                # gratings[stim_num].draw()
                 gratings[stim_num].draw()
                 # print frame_num, stim_num, clock.getTime(), last_stim_change
             # there's a hidden condition here, where we don't want to draw anything (and, in order
             # to make sure things line up exactly, we reset the last_stim_change and stim_num right
             # before)
-            elif frame_num >= (last_stim_change + on_frame_length + off_frame_length - 1):
+            elif frame_num >= (last_stim_change + expt_params['on_frame_length'] + expt_params['off_frame_length'] - 1):
                 last_stim_change = frame_num+1
                 stim_num += 1
                 # print frame_num, 'switching', clock.getTime(), last_stim_change
@@ -101,17 +135,41 @@ def main(stim_filename, monitor_kwargs={}, session_length=30, refresh_rate=60):
                 # print frame_num, '-', clock.getTime(), last_stim_change
         # else:
         #     print frame_num, 'waiting', clock.getTime()
-        if np.random.uniform() > .8 and frame_num > (last_fix_change + fix_change_min_length):
+        if frame_num >= (last_fix_change + next_fix_change):
             if (fixation.color == (0, 1, 0)).all():
                 fixation.color = (1, 0, 0)
             elif (fixation.color == (1, 0, 0)).all():
                 fixation.color = (0, 1, 0)
             last_fix_change = frame_num
+            next_fix_change = np.floor(np.random.uniform(*expt_params['fix_length_range']))
+            fixation_dot.append((frame_num, clock.getTime(), fixation.color))
         fixation.draw()
         win.flip()
         all_keys = event.getKeys(timeStamped=clock)
-        keys_pressed.extend((all_keys[0], all_keys[1], frame_num))
+        if all_keys:
+            keys_pressed.extend([(key[0], key[1], frame_num) for key in all_keys])
         if 'q' in [k[0] for k in all_keys] or 'escape' in [k[0] for k in all_keys]:
             break
     win.close()
-    return win, keys_pressed
+    return win, keys_pressed, fixation_dot
+
+
+def expt(stims_path, subj_name, **kwargs):
+    """run a full experiment
+
+    this just loops through the specified stims_path, passing each one to the run function in
+    turn. any other kwargs are sent directly to run as well. it then saves the returned
+    keys_pressed and frame intervals
+    """
+    file_path = "../data/raw_behavioral/%s_%s_sess{sess}.hdf5" % (datetime.datetime.now().strftime("%Y-%b-%d"), subj_name)
+    sess_num = 0
+    while glob.glob(file_path.format(sess=sess_num)):
+        sess_num += 1
+    for i, path in enumerate(stims_path):
+        win, keys = run(path, **kwargs)
+        with h5py.File(file_path.format(sess=sess_num), 'a') as f:
+            f.create_dataset("run_%s_button_presses" % i, data=np.array(keys))
+            if win is not None:
+                f.create_dataset("run_%s_frame_intervals" % i, data=np.array(win.frameIntervals))
+        if 'escape' in [k[0] for k in keys]:
+            break
