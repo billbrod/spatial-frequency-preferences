db0c153fe72d95b8111e5e046c74d3a7ee86c5ec
William F. Broderick <billbrod@gmail.com>
2017-10-14 16:02:10 -0400

    adds ability to plot image on x axis
    this moves the im_plot function from one of the notebooks into sfp.utils
    and adds a new function, add_img_to_xaxis, which adds an image onto the
    x axis with the specified size and position

--- sfp/utils.py
+++ sfp/utils.py
@@ -61,3 +61,36 @@ def scatter_ci_dist(x, y, ci_vals=[2.5, 97.5], **kwargs):
 def scatter_heat(x, y, c, **kwargs):
     plt.scatter(x, y, c=c, cmap='RdBu_r', s=50, norm=MidpointNormalize(midpoint=0),
                 vmin=kwargs['vmin'], vmax=kwargs['vmax'])
+
+
+def im_plot(im, **kwargs):
+    if 'ax' in kwargs:
+        ax = kwargs.pop('ax')
+        ax.imshow(im, cmap='gray', **kwargs)
+    else:
+        ax = plt.imshow(im, cmap='gray', **kwargs)
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+
+
+def add_img_to_xaxis(fig, ax, img, rel_position, size=.1):
+    """add image to x-axis
+
+    after calling this, you probably want to make your x axis invisible:
+    `ax.xaxis.set_visible(False)` or things will look confusing
+
+    rel_position: float between 0 and 1, specifies where on the x axis you want to place the
+    image. You'll need to play arond with this, I don't have a good way of doing this automatically
+    (for instance, lining up with existing tick marks). This interacts with size. if you want the
+    left edge to line up with the beginning of the x-axis, this should be 0, but if you want the
+    right edge to line up with the end of the x-axis, this should be around 1-size
+
+    size: float between 0 and 1, size of image, relative to overall plot size. it appears that
+    around .1 or .2 is a good size.
+    """
+    xl, yl, xh, yh = np.array(ax.get_position()).ravel()
+    w = xh - xl
+
+    ax1 = fig.add_axes([xl + w*rel_position, yl-size, size, size])
+    ax1.axison = False
+    im_plot(img, ax=ax1)
