8bc0569e16ba2f82f7305b1041b4f4d50c37585d
William F. Broderick <billbrod@gmail.com>
2017-11-27 15:52:14 -0500

    adds some utils functions
    this adds a handful of utils functions from the 03-MRI-first-level
    notebook:
    
     - create_sin_cpd: create a full-screen 2d sine wave, with the specified
       frequency in cpd (for a given screen radius in degrees)
    
     - create_circle_mask: create a circular mask of the specified size at
       the specified location
    
     - fit_log_norm_ci: fit log norm across bootstraps and plot the
       resulting mean and confidence intervals

--- sfp/utils.py
+++ sfp/utils.py
@@ -98,6 +98,32 @@ def add_img_to_xaxis(fig, ax, img, rel_position, size=.1, **kwargs):
     im_plot(img, ax=ax1, **kwargs)
 
 
+def create_sin_cpd(size, freq_cpd, max_degree_rad=12):
+    """create a full 2d sine wave, with frequency in cpd
+
+    this converts the desired freq_cpd into the frequency shown in an image by using the
+    max_degree_rad, the radius of the image in degrees of visual angle.
+    """
+    x = np.array(range(size))/float(size)
+    x, _ = np.meshgrid(x, x)
+    freq_pix = freq_cpd / (size / (2*float(max_degree_rad)))
+    freq_screen = size * freq_pix
+    return np.sin(2*np.pi*x*freq_screen)
+
+
+def create_circle_mask(x, y, rad, size):
+    """create a circular mask
+
+    this returns a circular mask centered at pixel (x, y) with radius rad in a size by size
+    image. This can then be multiplied by an image of the same size to mask out everything else.
+    """
+    x_grid = np.array(range(size))
+    x_grid, y_grid = np.meshgrid(x_grid, x_grid)
+    mask = np.zeros((size, size))
+    mask[(x_grid - x)**2 + (y_grid - y)**2 <= rad**2] = 1
+    return mask
+
+
 def log_norm_pdf(x, a, mu, sigma):
     """the pdf of the log normal distribution, with a scale factor
     """
@@ -109,6 +135,12 @@ def fit_log_norm(x, y, **kwargs):
     """fit log norm to data and plot the result
 
     to be used with seaborn.FacetGrid.map_dataframe
+
+    x: string, column in data which contains the x values for this plot.
+
+    y: string, column in data which contains the x values for this plot.
+
+    kwargs must contain `data`, the DataFrame with data to plot.
     """
     data = kwargs.pop('data')
     plot_data = data.groupby(x)[y].mean()
@@ -117,3 +149,45 @@ def fit_log_norm(x, y, **kwargs):
     plt.plot(plot_data.index, log_norm_pdf(plot_data.index, *popt), **kwargs)
 
     # to plot ci band, use ax.fill_between(x, low, high, facecolor=color, alpha=.2)
+
+
+def fit_log_norm_ci(x, y, ci_vals=[2.5, 97.5], **kwargs):
+    """fit log norm to different bootstraps and plot the resulting mean and confidence interval.
+
+    to be used with seaborn.FacetGrid.map_dataframe.
+
+    because this goes through all the bootstraps and calculates their log normal tuning curves
+    separately, it's takes much more time than fit_log_norm_ci
+
+    the data passed here must contain a column named `bootstrap_num`, which specifies which number
+    bootstrap the observation corresponds to. Each value of bootstrap_num will be fit
+    separately. It's recommended (i.e., this function was written assuming), therefore, that your
+    data only contains one y value per value of bootstrap_num and value of x.
+
+    x: string, column in data which contains the x values for this plot.
+
+    y: string, column in data which contains the x values for this plot.
+
+    ci_vals: 2-tuple or list of length 2 of floats, optional. the min and max percentile you wish
+    to plot as a shaded region. For example, if you wish to plot the 95% confidence interval, then
+    ci_vals=[2.5, 97.5] (the default); if you wish to plot the 68%, then ci_vals=[16, 84].
+
+    kwargs must contain `data`, the DataFrame with data to plot.
+    """
+    data = kwargs.pop('data')
+    if 'color' in kwargs:
+        color = kwargs.pop('color')
+        kwargs['facecolor'] = color
+    lines = []
+    for boot in data.bootstrap_num.unique():
+        plot_data = data.groupby(x)[[y, 'bootstrap_num']].apply(lambda x, j: x[x.bootstrap_num==j], boot)
+        plot_idx = plot_data.index.get_level_values(x)
+        plot_vals = plot_data[y].values
+        popt, _ = sp.optimize.curve_fit(log_norm_pdf, plot_idx, plot_vals)
+        lines.append(log_norm_pdf(plot_idx, *popt))
+    lines = np.array(lines)
+    lines_mean = lines.mean(0)
+    cis = np.percentile(lines, ci_vals, 0)
+    plt.fill_between(plot_idx, cis[0], cis[1], alpha=.2, **kwargs)
+    plt.plot(plot_idx, lines_mean, color=color, **kwargs)
+    return lines
