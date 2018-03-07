7a7c0bb0bc0348caa6f4b9bb0ed18b38791f5fe0
William F. Broderick <billbrod@gmail.com>
2018-03-07 13:14:38 -0500

    adds plot_data to plotting
    with this commit, adds new function, plot_data to plotting.py. this
    function plots the amplitude estimates (with 68% confidence interval, by
    default) of the data as a function of the freq_space_distance (by
    default, can attempt to do it with anything). as part of this, adds
    ability to call this function from the command line and also moves a
    whole bunch of plotting helper functions from utils.py to plotting.py

--- sfp/utils.py
+++ sfp/utils.py
@@ -2,7 +2,6 @@
 """various utils
 """
 
-import matplotlib
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,62 +12,11 @@ import pandas as pd
 import first_level_analysis
 import tuning_curves
 import warnings
-
-
-class MidpointNormalize(matplotlib.colors.Normalize):
-    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
-        self.midpoint = midpoint
-        matplotlib.colors.Normalize.__init__(self, vmin, vmax, clip)
-
-    def __call__(self, value, clip=None):
-        # I'm ignoring masked values and all kinds of edge cases to make a
-        # simple example...
-        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
-        return np.ma.masked_array(np.interp(value, x, y))
-
-
-def plot_mean(x, y, **kwargs):
-    """plot line through center points, for use with seaborn's map_dataframe
-    """
-    data = kwargs.pop('data')
-    plot_data = data.groupby(x)[y].mean()
-    plt.plot(plot_data.index, plot_data.values, **kwargs)
-
-
-def scatter_ci_col(x, y, ci, **kwargs):
-    """plot center points and specified CIs, for use with seaborn's map_dataframe
-
-    based on seaborn.linearmodels.scatterplot. CIs are taken from a column in this function.
-    """
-    data = kwargs.pop('data')
-    plot_data = data.groupby(x)[y].mean()
-    plot_cis = data.groupby(x)[ci].mean()
-    plt.scatter(plot_data.index, plot_data.values, **kwargs)
-    for (x, ci), y in zip(plot_cis.iteritems(), plot_data.values):
-        plt.plot([x, x], [y+ci, y-ci], **kwargs)
-
-
-def scatter_ci_dist(x, y, ci_vals=[2.5, 97.5], **kwargs):
-    """plot center points and specified CIs, for use with seaborn's map_dataframe
-
-    based on seaborn.linearmodels.scatterplot. CIs are taken from a distribution in this
-    function. Therefore, it's assumed that the values being passed to it are values from a
-    bootstrap distribution.
-
-    by default, this draws the 95% confidence interval. to change this, change the ci_vals
-    argument. for instance, if you only want to draw the mean point, pass ci_vals=[50, 50] (this is
-    eqiuvalent to just calling plt.scatter)
-    """
-    data = kwargs.pop('data')
-    plot_data = data.groupby(x)[y].mean()
-    plot_cis = data.groupby(x)[y].apply(np.percentile, ci_vals)
-    plt.scatter(plot_data.index, plot_data.values, **kwargs)
-    for x, (ci_low, ci_high) in plot_cis.iteritems():
-        plt.plot([x, x], [ci_low, ci_high], **kwargs)
+import plotting
 
 
 def scatter_heat(x, y, c, **kwargs):
-    plt.scatter(x, y, c=c, cmap='RdBu_r', s=50, norm=MidpointNormalize(midpoint=0),
+    plt.scatter(x, y, c=c, cmap='RdBu_r', s=50, norm=plotting.MidpointNormalize(midpoint=0),
                 vmin=kwargs['vmin'], vmax=kwargs['vmax'])
 
 
