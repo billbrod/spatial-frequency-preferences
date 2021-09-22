501f9e029daae64f62870fc80e42dd7a1a3f2cc8
William F. Broderick <billbrod@gmail.com>
2021-09-22 10:56:37 -0400

    adds extra to download_data.py

--- download_data.py
+++ download_data.py
@@ -26,13 +26,22 @@ def main(target_dataset):
         subprocess.call(["rsync", "-avPLuz", "derivatives/", f"{deriv_folder}/"])
         subprocess.call(["rm", "-r", "derivatives/"])
         subprocess.call(["rm", "sfp_fully_processed_data.tar.gz"])
+    elif target_dataset == 'extra':
+        print("Downloading fully-processed data.")
+        subprocess.call(["curl", "-O", "-J", "-L", "https://osf.io/b3cdj/download"])
+        subprocess.call(["tar", "xf", "sfp_extra_data.tar.gz"])
+        subprocess.call(["rsync", "-avPLuz", "derivatives/", f"{deriv_folder}/"])
+        subprocess.call(["rm", "-r", "derivatives/"])
+        subprocess.call(["rm", "sfp_extra_data.tar.gz"])
+
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=("Download the spatial frequency preferences dataset, to reproduce the results.")
     )
-    parser.add_argument("target_dataset", choices=['preprocessed', 'partially', 'fully'],
+    parser.add_argument("target_dataset", choices=['preprocessed', 'partially', 'fully', 'extra'],
                         help="Which dataset to download, see project README for details.")
     args = vars(parser.parse_args())
     main(**args)
