293e4f80fd3e689518b952e5250132fcd161a4de
William F. Broderick <billbrod@gmail.com>
2022-01-05 11:28:47 -0500

    adds some comments for containers

--- singularity_env.sh
+++ singularity_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# set up environment variables for other libraries
+# set up environment variables for other libraries, add them to path
 export FREESURFER_HOME=/home/sfp_user/freesurfer
 export PATH=$FREESURFER_HOME/bin:$PATH
 
@@ -10,6 +10,8 @@ export FSLOUTPUTTYPE=NIFTI_GZ
 export FSLDIR=/home/sfp_user/fsl
 export PATH=$FSLDIR/bin:$PATH
 
+# modify the config.json file so it points to the location of MRI_tools,
+# GLMDenoise, and Vistasoft within the container
 if [ -f /home/sfp_user/spatial-frequency-preferences/config.json ]; then
     cp /home/sfp_user/spatial-frequency-preferences/config.json /home/sfp_user/sfp_config.json
     sed -i 's|"MRI_TOOLS":.*|"MRI_TOOLS": "/home/sfp_user/MRI_tools",|g' /home/sfp_user/sfp_config.json
