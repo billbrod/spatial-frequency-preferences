6f1aea50880e583e6fd027023cba09fd18d87882
William F. Broderick <billbrod@gmail.com>
2024-09-12 12:09:02 -0400

    update default openneuro data version

--- download_data.py
+++ download_data.py
@@ -8,7 +8,7 @@ import json
 from glob import glob
 
 
-def main(target_dataset, preprocessed_version='1.0.0'):
+def main(target_dataset, preprocessed_version='1.1.0'):
     """Download dataset from OpenNeuro or OSF.
 
     Parameters
@@ -103,7 +103,7 @@ if __name__ == '__main__':
                                                    'fully-processed',
                                                    'supplemental'],
                         help="Which dataset to download, see project README for details.")
-    parser.add_argument("--preprocessed_version", default='1.0.0',
+    parser.add_argument("--preprocessed_version", default='1.1.0',
                         help="Which version fo the preprocessed dataset from openneuro to download")
     args = vars(parser.parse_args())
     main(**args)
